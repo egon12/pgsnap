7af984ce014ec4d5ea37e09e8732d891761b77f8
Egon Firman <egon.firman@tokopedia.com>
2023-04-11 09:09:20 +0700

    fix panic when proxy log after test finish
    As a proxy, there are function that log, when something is happened
    Now when we are using dockertest when run the test, sometimes
    the postgres container is finish after the test is done.
    
    And this is call will cause panic cause it will call log in *testing.T
    This commit will check isDone() first before logging. Cause disconnect
    after test is finish is kind expected.

--- proxy.go
+++ proxy.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"io"
+	"log"
 	"net"
 	"os"
+	"sync"
 	"sync/atomic"
 	"testing"
 
@@ -14,12 +16,13 @@ import (
 )
 
 type proxy struct {
-	t       testing.TB
-	dsn     string
-	script  *script
-	l       net.Listener
-	isDebug bool
-	done    atomic.Bool
+	t         testing.TB
+	dsn       string
+	script    *script
+	l         net.Listener
+	isDebug   bool
+	done      atomic.Bool
+	doneMutex sync.Mutex
 }
 
 func newProxy(t testing.TB, dsn string, script *script, l net.Listener, isDebug bool) *proxy {
@@ -45,9 +48,10 @@ func (s *proxy) run() {
 	if err != nil {
 		s.t.Fatalf("can't connect to db %s: %v", s.dsn, err)
 	}
+
 	err = db.Ping(context.TODO())
 	if err != nil {
-		s.t.Fatalf("can't pint to db %s: %v", s.dsn, err)
+		s.t.Fatalf("can't ping to db %s: %v", s.dsn, err)
 	}
 
 	// only accept one connection / test. This is a limitation of the current
@@ -60,7 +64,8 @@ func (s *proxy) finish() {
 		return
 	}
 
-	s.done.Store(false)
+	s.debugLogf("pgsnap: proxy finish")
+	s.setDone()
 }
 
 func (s *proxy) acceptConnForProxy(db *pgx.Conn, out io.Writer) {
@@ -80,6 +85,7 @@ func (s *proxy) acceptConnForProxy(db *pgx.Conn, out io.Writer) {
 	s.runConversation(fe, be, out)
 }
 
+// runConversation will run conversation between frontend and backend
 func (s *proxy) runConversation(fe *pgproto3.Frontend, be *pgproto3.Backend, out io.Writer) {
 	go s.streamBEtoFE(fe, be, out)
 	go s.streamFEtoBE(fe, be, out)
@@ -91,12 +97,16 @@ func (s *proxy) streamBEtoFE(fe *pgproto3.Frontend, be *pgproto3.Backend, out io
 	for {
 		s.debugLogf("pgsnap: BE receiving")
 		msg, err := be.Receive()
-		if err == io.ErrUnexpectedEOF {
-			s.t.Errorf("pgsnap: BE got unexpectedEOF. Maybe db exited?")
-			return
-		}
-
 		if err != nil {
+			if s.isDone() {
+				s.debugLogf("pgsnap: error on receive after test done: %v", err)
+				return
+			}
+			if err == io.ErrUnexpectedEOF {
+				s.t.Errorf("pgsnap: BE got unexpectedEOF. Maybe db client exited?")
+				return
+			}
+
 			s.t.Errorf("pgsnap: BE failed receive message: %v", err)
 			continue
 		}
@@ -115,21 +125,13 @@ func (s *proxy) streamBEtoFE(fe *pgproto3.Frontend, be *pgproto3.Backend, out io
 		if msg != nil {
 			s.debugLogf("pgsnap: BE send to database: %+v", msg)
 			err = fe.Send(msg)
-			if s.isDebug {
-				s.t.Logf("pgsnap: BE send to database done err: %v", err)
-			}
+			s.debugLogf("pgsnap: BE send to database done err: %v", err)
 			if err != nil {
 				s.t.Errorf("pgsnap: BE cannot forward to postgre: %T: %+v", msg, msg)
 			}
 		}
 
-		if _, ok := msg.(*pgproto3.Terminate); ok {
-			s.debugLogf("pgsnap: BE got terminate")
-			s.done.Store(true)
-			return
-		}
-
-		if s.done.Load() {
+		if s.isDone() {
 			s.debugLogf("pgsnap: BE exit loop")
 			return
 		}
@@ -141,23 +143,26 @@ func (s *proxy) streamFEtoBE(fe *pgproto3.Frontend, be *pgproto3.Backend, out io
 		s.debugLogf("pgsnap: FE receiving")
 
 		msg, err := fe.Receive()
-		if err == io.ErrUnexpectedEOF {
-			if s.done.Load() {
-				s.debugLogf("pgsnap: FE got EOF exit")
+		if err != nil {
+			if s.isDone() {
+				s.debugLogf("pgsnap: FE loop exit, error after done: %v", err)
 				return
 			}
-			s.t.Errorf("pgsnap: unexpectedEOF from Database. Maybe database exit unexpectedly?")
-			return
-		}
-		if err != nil {
+
+			if err == io.ErrUnexpectedEOF {
+				s.t.Errorf("pgsnap: unexpectedEOF from Database. Maybe database exit unexpectedly?")
+				return
+			}
+
 			s.t.Errorf("pgsnap: error when FE receive: %v", err)
 			continue
 		}
-		s.debugLogf("pgsnap: FE recive BE message %T: %+v", msg, msg)
+
+		s.debugLogf("pgsnap: FE receive Database message %T: %+v", msg, msg)
 
 		b, err := json.Marshal(msg)
 		if err != nil {
-			s.t.Errorf("pgsnap: FE cannot marshal BE message: %T: %+v", msg, msg)
+			s.t.Errorf("pgsnap: FE cannot marshal Database message: %T: %+v", msg, msg)
 		}
 		if len(b) > 0 {
 			b = append([]byte{'B', ' '}, b...)
@@ -173,7 +178,7 @@ func (s *proxy) streamFEtoBE(fe *pgproto3.Frontend, be *pgproto3.Backend, out io
 			}
 		}
 
-		if s.done.Load() {
+		if s.isDone() {
 			s.debugLogf("pgsnap: FE exit loop")
 			return
 		}
@@ -199,6 +204,23 @@ func (s *proxy) prepareFrontend(db *pgx.Conn) *pgproto3.Frontend {
 
 func (s *proxy) debugLogf(format string, args ...interface{}) {
 	if s.isDebug {
-		s.t.Logf(format, args...)
+		if !s.isDone() {
+			s.t.Helper()
+			s.t.Logf(format, args...)
+			return
+		}
+		log.Printf(s.t.Name()+": "+format, args...)
 	}
+}
+
+func (s *proxy) setDone() {
+	s.doneMutex.Lock()
+	defer s.doneMutex.Unlock()
+	s.done.Store(true)
+}
+
+func (s *proxy) isDone() bool {
+	s.doneMutex.Lock()
+	defer s.doneMutex.Unlock()
+	return s.done.Load()
 }
